f43f9fe49e83cd05e201fddcde754bc84679b75a
dongmeic <dongmeijane1107@gmail.com>
2018-12-08 17:46:55 -0800

    discard fire containment costs as a predictor

--- R/containment_costs.R
+++ R/containment_costs.R
@@ -1,9 +1,11 @@
+# Discard fire containment costs as a predictor
 # libraries
 library(sp)
 library(rgdal)
 library(raster)
 library(data.table)
 library(maptools)
+library(rgeos)
 
 # region of interest
 mpb10km <- readOGR(dsn = "/gpfs/projects/gavingrp/dongmeic/beetle/shapefiles/mpb10km", layer = "mpb10km")
@@ -16,7 +18,11 @@ crs <- proj4string(mpb10km)
 
 # functions
 mode <- function(x) {
-  return(names(sort(-table(x)))[1])
+  return(names(sort(-table(na.omit(x))))[1])
+}
+
+MAX <- function(x){
+	max(x, na.rm=TRUE)
 }
 
 df2spdf <- function(col1, col2, colname1, colname2, df){
@@ -81,7 +87,7 @@ print(paste("SIT-209 has", length(merge$SITID), "records during", range(merge$Ye
 # "SIT-209 has 104400 records during 1999 and 2016 in raw data where percent containment is 100!"
 # write.csv(merge, paste0(fire.path,"ics-209.csv"), row.names = FALSE)
 icsdata.a <- aggregate(cbind(Longitude, Latitude, Year, State) ~ SITID, data = merge, mode)
-icsdata.b <- aggregate(cbind(Costs, Acres) ~ SITID, data = merge, max)
+icsdata.b <- aggregate(cbind(Costs, Acres) ~ SITID, data = merge, MAX)
 icsdata.c <- aggregate(StartDate ~ SITID, data=merge, function(x) min(as.Date(x, format = "%m/%d/%Y")))
 icsdata.d <- aggregate(EndDate ~ SITID, data=merge, function(x) max(as.Date(x, format = "%m/%d/%Y")))
 icsdata.1 <- merge(icsdata.a, icsdata.b, by = "SITID", all=TRUE)
@@ -97,6 +103,47 @@ icsdata.n1$LogCost <- log(icsdata.n1$Costs)
 icsdata.n1$LogAcre <- log(icsdata.n1$Acre)
 #write.csv(icsdata.n1, paste0(fire.path,"ics-209-costs-aggregate.csv")) # this table is used to join table in ArcGIS
 colnames(icsdata.n)[1] <- "ICS_209_INCIDENT_NUMBER"
+# remove zeros in coordinates
+icsdata.n.1 <- with(icsdata.n, icsdata.n[!(Longitude == 0), ])
+icsdata.n.2 <- with(icsdata.n.1, icsdata.n.1[!(Latitude == 0), ])
+icsdata.n.3 <- icsdata.n.2[!is.na(icsdata.n.2$Longitude) & !is.na(icsdata.n.2$Latitude),]
+icsdata.n.3$Longitude <- as.numeric(icsdata.n.3$Longitude)
+icsdata.n.3$Latitude <- as.numeric(icsdata.n.3$Latitude)
+# length(icsdata.n.3$ICS_209_INCIDENT_NUMBER)
+# [1] 23112
+# save points in contiguous US
+icsdata.n.3 <- icsdata.n.3[icsdata.n.3$Longitude > -125 & icsdata.n.3$Longitude < -66 
+                           & icsdata.n.3$Latitude > 24 & icsdata.n.3$Latitude < 50, ] 
+# length(icsdata.n.3$ICS_209_INCIDENT_NUMBER)
+# [1] 22531
+sit.spdf <- df2spdf(2,3,"Longitude", "Latitude",icsdata.n.3)
+length(na.omit(sit.spdf@data$Costs))
+sit.westus <- gIntersection(sit.spdf, mpb10km)
+png(paste0(out,"sit_209_westUS.png"), width = 10, height = 8, units = "in", res=300)
+par(mfrow=c(1,1),xpd=FALSE,mar=c(0,0,2,0))
+plot(mpb10km, main="SIT-209 fire records in the western US")
+points(sit.westus$x, sit.westus$y, pch=16, col="red", cex=0.2)
+dev.off()
+sit_westus <- sit.spdf[!is.na(over(sit.spdf, as(mpb10km, "SpatialPolygons"))),]
+writeOGR(sit_westus, dsn = fire.path, layer = "sit_westus", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+length(na.omit(sit_westus@data$Costs)); length(na.omit(sit_westus@data$Acres))
+#[1] 6757
+#[1] 6757
+sit_westus <- sit_westus[!is.na(sit_westus$Costs) & !is.na(sit_westus$Acres),]
+sit_westus <- sit_westus[sit_westus$Costs > 0 & sit_westus$Acres > 0,]
+sit_westus@data$CostPerAcre <- sit_westus@data$Costs/sit_westus@data$Acres
+par(mfrow=c(1,1),xpd=FALSE,mar=c(4,4,2,1))
+hist(sit_westus@data$Costs[sit_westus@data$Costs<100000000])
+quantile(sit_westus@data$Costs, c(.05, .5, 0.75, 0.9, .95))
+hist(sit_westus@data$CostPerAcre[sit_westus@data$CostPerAcre < 50000])
+
+sit_westus <- sit_westus[sit_westus$Costs<100000000,]
+cell.size <- 10000
+xmin <- -1006739; xmax <- 1050000.0; ymin <- -1722656; ymax <- 539131.6
+ncols <- (xmax - xmin)/cell.size; nrows <- (ymax - ymin)/cell.size
+r <- raster(nrows=nrows, ncols=ncols, ext=extent(mpb10km),crs = crs)
+cost <- rasterize(sit_westus, r, "Costs", fun=mean, na.rm=TRUE) 
+costPerAcre <- rasterize(sit_westus, r, "CostPerAcre", fun=mean, na.rm=TRUE)
 
 # MTBS
 mtbs.shp <- readOGR(dsn=paste0(fire.path, "mtbs_perimeter_data"), layer="mtbs_perims_DD")
@@ -136,7 +183,7 @@ fpa.sit.df$Duration3 <- as.numeric(difftime(as.Date(fpa.sit.df$EndDate), as.Date
 fpa.sit.df$Duration <- ifelse(fpa.sit.df$Duration1 > 200 | fpa.sit.df$Duration1 < 0, abs(fpa.sit.df$Duration2), fpa.sit.df$Duration1)
 fpa.sit.df$Duration <- ifelse(fpa.sit.df$Duration1 == 0 & fpa.sit.df$Duration2 == 0 & fpa.sit.df$Duration3 > 0 & fpa.sit.df$Duration3 < 200, fpa.sit.df$Duration3, fpa.sit.df$Duration)
 # get a table to join in ArcGIS
-fpa.sit.tb <- fpa.sit.df[!is.na(ICS_209_INCIDENT_NUMBER) & !is.na(MTBS_ID),]
+fpa.sit.tb <- fpa.sit.df[!is.na(MTBS_ID),]
 setkey(fpa.sit.tb, "MTBS_ID")
 length(fpa.sit.tb$MTBS_ID);length(unique(fpa.sit.tb$MTBS_ID))
 #[1] 6633
@@ -151,5 +198,12 @@ fpa.sit.tb.1 <- fpa.sit.tb[, list(ICS_209_INCIDENT_NUMBER = first(ICS_209_INCIDE
 length(fpa.sit.tb.1$MTBS_ID);length(unique(fpa.sit.tb.1$MTBS_ID))
 #[1] 6490
 #[1] 6490
-
-
+colnames(fpa.sit.tb.1)[which(names(fpa.sit.tb.1) == "Acres")] <- "SIT_Acres" # differentiate the acres from MTBS
+colnames(fpa.sit.tb.1)[which(names(fpa.sit.tb.1) == "Year")] <- "SIT_Year"
+fpa.sit.mtbs.df <- merge(fpa.sit.tb.1, mtbs.df, by= "MTBS_ID", all=TRUE)
+print(colnames(fpa.sit.mtbs.df))
+head(fpa.sit.mtbs.df)
+length(na.omit(fpa.sit.mtbs.df$Costs))
+length(na.omit(fpa.sit.df$Costs))
+length(na.omit(fpa.sit.tb$Costs))
+write.csv(fpa.sit.mtbs.df, paste0(fire.path,"fpa_sit_mtbs.csv"), row.names = FALSE)
