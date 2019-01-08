4e1891ba191dce13e13a7d2a1ab736e4b3454a75
dongmeic <dongmeijane1107@gmail.com>
2019-01-08 15:35:33 -0800

    mapping beetle affected acres

---
+++ R/mapping_beetle_affected_acres.R
@@ -0,0 +1,59 @@
+
+setwd('/gpfs/projects/gavingrp/dongmeic/beetle/output/maps')
+csvpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/"
+df <- read.csv(paste0(csvpath, "mpb10km_nonclimate.csv"))
+
+shppath <- "/gpfs/projects/gavingrp/dongmeic/beetle/shapefiles/mpb10km"
+mpb10km <- readOGR(dsn = shppath, layer = "mpb10km")
+crs <- proj4string(mpb10km)
+
+get.spdf <- function(df, year){
+	ndf <- df[,c('x', 'y', grep(year, colnames(df), value=TRUE))]
+	xy <- data.frame(ndf[,c(1,2)])
+	coordinates(xy) <- c('x', 'y')
+	proj4string(xy) <- crs
+	spdf <- SpatialPointsDataFrame(coords = xy, data = ndf, proj4string = crs)
+	return(spdf)
+}
+
+nclr <- 5
+color <- "YlOrBr"
+plotclr <- brewer.pal(nclr,color)
+
+mpb_acres_mapping <- function(df, year){
+	spdf <- get.spdf(df, year)
+	plotvar <- log(as.numeric(spdf@data[,3])+1)
+	class <- classIntervals(plotvar, nclr, style="kmeans", dataPrecision=1)
+	colcode <- findColours(class, plotclr)
+	png(paste0("MPB_",year,"_test.png"), width=5, height=5, units="in", res=300)
+	par(mfrow=c(1,1),mar=c(0.5,0,1.5,0))
+	plot(spdf, col=colcode, main=year, pch=19, cex=0.1)
+	plot(mpb10km, bord=rgb(0.7,0.7,0.7,0.7), add=T)
+	legend('bottomleft',legend=names(attr(colcode, "table")),
+						 fill=attr(colcode, "palette"), cex=1.2, title='', bty="n")
+	dev.off()
+}
+
+mpb_acres_mapping(df, 1997)	
+			 
+mpb_acres_ts <- function(df, outnm){
+	png(paste0(outnm, ".png"), width=15, height=12, units="in", res=300)
+	par(mfrow=c(4,5),mar=c(0.5,0.5,1.5,0))
+	for (year in 1997:2016){
+		spdf <- get.spdf(df, year)
+		plotvar <- log(as.numeric(spdf@data[,3])+1)
+		class <- classIntervals(plotvar, nclr, style="kmeans", dataPrecision=1)
+		colcode <- findColours(class, plotclr)
+		plot(spdf, col=colcode, pch=19, cex=0.1)
+		title(main=year, adj = 0.5, line = -1, cex.main=2)
+		plot(mpb10km, bord=rgb(0.7,0.7,0.7,0.7), add=T)
+		legend('bottomleft',legend=names(attr(colcode, "table")),
+						 fill=attr(colcode, "palette"), title='', bty="n")
+		print(year)
+	}	
+	dev.off()
+}
+
+mpb_acres_ts(df, 'MPB_acres_ts')
+
+
