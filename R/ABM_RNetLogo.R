890901b65b236c53b47b4f58cca2603a93806985
dchen <dongmeic@uoregon.edu>
2019-01-29 17:14:19 -0800

    R NetLogo

---
+++ R/ABM_RNetLogo.R
@@ -0,0 +1,8 @@
+#install.packages('rJava')
+library(rJava)
+library(RNetLogo)
+if (Sys.info()['sysname'] == 'Darwin') {
+  libjvm <- paste0(system2('/usr/libexec/java_home',stdout = TRUE)[1],'/jre/lib/server/libjvm.dylib')
+  message (paste0('Load libjvm.dylib from: ',libjvm))
+  dyn.load(libjvm)
+}
