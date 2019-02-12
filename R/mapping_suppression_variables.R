4a5164fb4a98bf55a6606709fe2cf24a3efd0533
dongmeic <dongmeijane1107@gmail.com>
2019-02-12 09:46:40 -0800

    mapping fire suppression costs

--- R/mapping_suppression_variables.R
+++ R/mapping_suppression_variables.R
@@ -1,5 +1,6 @@
 library(rasterVis)
 library(colorRamps)
+library(BAMMtools)
 
 source("/gpfs/projects/gavingrp/dongmeic/suppression/R/data_summary_functions.R")
 csvpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/"
@@ -16,18 +17,18 @@ spdf <- df2spdf(1, 2, 'lon', 'lat', df)
 
 mapping.LF <- function(shp, var){	
 	if(var %in% c('pls', 'pms', 'prs')){
-		labels <- c("0-5%", "6-10%", "11-15%", "16-20%", "21-25%", "26-30%",
-						"31-35%", "36-40%", "41-45%", "46-50%", "51-55%", "56-60%",
-						"61-65%", "66-70%", "71-75%", "76-80%", "81-85%", "86-90%", "91-95%", "96-100%")
+		labels <- c("0-5", "6-10", "11-15", "16-20", "21-25", "26-30",
+						"31-35", "36-40", "41-45", "46-50", "51-55", "56-60",
+						"61-65", "66-70", "71-75", "76-80", "81-85", "86-90", "91-95", "96-100")
 		if(var == 'pls'){
-			title <- "Percent of low-severity fires"
+			title <- "Percent of low-severity fires (%)"
 		}else if(var == 'pms'){
-			title <- "Percent of mixed-severity fires"
+			title <- "Percent of mixed-severity fires (%)"
 		}else{
-			title <- "Percent of replacement-severity fires"
-			labels <- c("0-5%", "6-10%", "11-15%", "16-20%", "21-25%", "26-30%",
-						"31-35%", "36-40%", "41-45%", "46-50%", "51-55%", "56-60%",
-						"61-65%", "66-70%", "71-75%", "76-80%", "81-85%", "86-90%", "96-100%")
+			title <- "Percent of replacement-severity fires (%)"
+			labels <- c("0-5", "6-10", "11-15", "16-20", "21-25", "26-30",
+						"31-35", "36-40", "41-45", "46-50", "51-55", "56-60",
+						"61-65", "66-70", "71-75", "76-80", "81-85", "86-90", "96-100")
 		}
 		cols <- matlab.like(20)
 	}else if(var == 'mfri'){
@@ -36,11 +37,11 @@ mapping.LF <- function(shp, var){
 						"71-80", "81-90", "91-100", "101-125", "126-150", "151-200",
 						"201-300", "301-500", "501-1000", ">1000")
 		cols <- rev(matlab.like(22))
-		title <- "Mean fire return interval"
+		title <- "Mean fire return interval (Years)"
 	}else if(var == 'vcc'){
-		labels <- c("0-16%", "17-33%", "34-50%", "51-66%", "67-83%", "84-100%")
+		labels <- c("0-16", "17-33", "34-50", "51-66", "67-83", "84-100")
 		cols <- brewer.pal(7,'BuGn')[-1]
-		title <- "Vegetation condition class"
+		title <- "Vegetation condition class (%)"
 	}
 	shp <- shp[shp@data[,var] > 0,]
 	r <- rasterize(shp, mpb10km.pts.r, var, fun=mean, na.rm=TRUE) 
@@ -54,18 +55,35 @@ mapping.LF <- function(shp, var){
 	return(p)
 }
 
+mapping.sprs <- function(shp, var, title, cols="YlOrRd"){	
+	shp <- shp[shp@data[,var] != Inf & !is.na(shp@data[,var]),]
+	qt99 <- quantile(shp@data[,var], 0.99)
+	shp <- shp[shp@data[,var] <= qt99,]
+	r <- rasterize(shp, mpb10km.pts.r, var, fun=mean, na.rm=TRUE)
+	ncls <- 6
+	brks <- getJenksBreaks(getValues(r), ncls-1)
+	p <- levelplot(r, col.regions=brewer.pal(ncls,cols)[-1], cuts=ncls, at=brks, xlab="", ylab="", par.settings = list(axis.line = list(col = "transparent")), 
+						scales = list(draw = FALSE), margin=F, main=title)
+	p <- p + latticeExtra::layer(sp.polygons(mpb10km, lwd=0.5, col=alpha("black", alpha = 0.6)))
+	return(p)
+}
 
 
 shp <- spdf
-var <- 'vcc'
-var <- 'mfri'
+
+var <- 'SprsCPA'
+title <- 'Suppression costs per acre ($)'
 
 mapping.LF(shp, 'vcc')
 mapping.LF(shp, 'mfri')
 mapping.LF(shp, 'pls')
 mapping.LF(shp, 'pms')
 mapping.LF(shp, 'prs')
-
+mapping.sprs(shp, 'SprsCPA', 'Suppression costs per acre ($)')
+mapping.sprs(shp, 'SprsCosts', 'Suppression costs ($)')
+mapping.sprs(shp, 'SprsAcres', 'Suppression acres')
+mapping.sprs(shp, 'SprsDays', 'Containment duration (Days)')
+mapping.sprs(shp, 'OutDays', 'Fire out duration (Days)')
 
 
 
