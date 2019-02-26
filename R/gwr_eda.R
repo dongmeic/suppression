5ffabc514b901e1f5db72b3b9138aa3e6997f446
dongmeic <dongmeijane1107@gmail.com>
2019-02-26 01:30:19 -0800

    gwr eda

--- R/gwr_eda.R
+++ R/gwr_eda.R
@@ -1,17 +1,26 @@
 library(spgwr)
 
 ROOT <- '/gpfs/projects/gavingrp/dongmeic/beetle/output/tables'
-data <- read.csv(sprintf('%s/mpb10km_data_wo_FIA_scaled.csv', ROOT))
+data <- read.csv(sprintf('%s/mpb10km_data_wo_FIA.csv', ROOT))
 xy <- read.csv(sprintf('%s/mpb10km_data_wo_FIA_xy.csv', ROOT))
 
 ptm <- proc.time()
-GWRbandwidth <- gwr.sel(beetleAcres ~ ., data=data, coords=cbind(xy$x,xy$y), adapt=T)
+vars <- c()
+GWRbandwidth <- gwr.sel(log(beetleAcres) ~ lon + lat + etopo1 + mStdAge + density + 
+    prs + mfri + GAP1 + GAP2 + GAP3 + vpd + summerP0 + Tmean + cwd + Tvar + 
+    wd + AugTmax + Acs + PPT + Mar20, data=data, coords=cbind(xy$x,xy$y), adapt=T)
 proc.time() - ptm
 
 ptm <- proc.time()
-gwr.model <- gwr(beetleAcres ~ ., data=data, coords=cbind(xy$x, xy$y), adapt=GWRbandwidth, 
-                hatmatrix=TRUE, se.fit=TRUE)
+gwr.model <- gwr(log(beetleAcres) ~ lon + lat + etopo1 + mStdAge + density + 
+    prs + mfri + GAP1 + GAP2 + GAP3 + vpd + summerP0 + Tmean + cwd + Tvar + 
+    wd + AugTmax + Acs + PPT + Mar20, data=data, coords=cbind(xy$x, xy$y), 
+    bandwidth=GWRbandwidth, adapt=T, hatmatrix=TRUE, se.fit=TRUE)
 proc.time() - ptm
 
+sink(sprintf('%s/gwr_model_results.txt', ROOT))
+gwr.model
+sink()
+
 results <- as.data.frame(gwr.model$SDF)
-write.csv(results, sprintf('%s/gwr_results_scaled.csv', ROOT), row.names=FALSE)
+write.csv(results, sprintf('%s/gwr_results_wo_FIA_20v.csv', ROOT), row.names=FALSE)
---
+++ R/gwr_eda_simple.R
@@ -0,0 +1,24 @@
+library(spgwr)
+
+ROOT <- '/gpfs/projects/gavingrp/dongmeic/beetle/output/tables'
+data <- read.csv(sprintf('%s/mpb10km_data_wo_FIA.csv', ROOT))
+xy <- read.csv(sprintf('%s/mpb10km_data_wo_FIA_xy.csv', ROOT))
+
+vars <- c('mStdAge', 'density', 'prs', 'mfri', 'vpd', 'summerP0', 'cwd', 'Tvar',
+					'wd', 'AugTmax', 'Acs', 'PPT', 'Mar20', 'ddAugJul')
+					
+ptm <- proc.time()
+for(v in vars){	
+	df <- data[, c('beetleAcres', v)]
+	colnames(df)[2] <- 'var'
+	GWRbandwidth <- gwr.sel(log(beetleAcres) ~ var, data=df, coords=cbind(xy$x,xy$y), adapt=T)
+	gwr.model <- gwr(log(beetleAcres) ~ var, data=df, coords=cbind(xy$x, xy$y), 
+									 bandwidth=GWRbandwidth, adapt=T, hatmatrix=TRUE, se.fit=TRUE)
+	
+	sink(sprintf('%s/gwr_model_results_%s.txt', ROOT, v))
+	gwr.model
+	sink()
+	results <- as.data.frame(gwr.model$SDF)
+	write.csv(results, sprintf('%s/gwr_results_wo_FIA_%s.csv', ROOT, v), row.names=FALSE)
+}
+proc.time() - ptm
